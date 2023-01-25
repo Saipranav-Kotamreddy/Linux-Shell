2070cf3b0e9bd58b5dd77d9a554bff79815de76b
MohammadAhmed108 <122654720+MohammadAhmed108@users.noreply.github.com>
2023-01-24 22:35:14 -0800

    Update Whitespace.h

--- Whitespace.h
+++ Whitespace.h
@@ -1,47 +1,102 @@
-#include <stdio.h>
-#include <string.h>
+ #include <stdio.h>
+ #include <string.h>
 
-char *noSpace(char* str_1);
+ char *noSpace(char* str_1);
+
 
 char *noSpace(char* str_1){
   int L = strlen(str_1);
-    char* str_new = malloc(1024);
-    memset(str_new, '\0', 1024);
-    char str_temp[1024]= "";
-    int x = 0;
-    int y= 0;
-    for(int i = 0; i < L; i++) {
-        if(str_1[i] == '>' || str_1[i] == '|'){
-          if(str_1[i] == '>'){
-            if((str_1[i-1] != ' ') || (str_1[i+1] != ' ')){
-              str_temp[x] = ' ';
-              x++;
-            }
-          }
-          str_temp[x] = str_1[i];
-          x++;
-          if(str_1[i] == '>'){
-            if((str_1[i-1] != ' ') || (str_1[i+1] != ' ')){
-              str_temp[x] = ' ';
-              x++;
-            }
-          }
-        }
-        else if(str_temp[x] == ' ' && str_1[i] == ' '){
-            continue;
-          }
-        else { 
-            str_temp[x] = str_1[i];
-           // printf("%c", str_temp[x]);
-            str_new[y] = str_temp[x];
-            y++;
-          }
-
+  char* str_new = malloc(1024);
+  memset(str_new, '\0', 1024);
+  int x = 0;
+  int y= 0;
+  for(int i = 0; i < L; i++) {
+    // check for '>' or '>>'
+    if (str_1[i] == '>') {
+      if (i > 0 && str_1[i - 1] != ' ') {
+        str_new[y] = ' ';
+        y++;
+      }
+      str_new[y] = str_1[i];
+      y++;
+      if (i < L - 1 && str_1[i + 1] != ' ') {
+        str_new[y] = ' ';
+        y++;
+      }
     }
-
-    //printf("%s\n", str_new);
-    return str_new; 
+    // check for '|'
+    else if (str_1[i] == '|') {
+      str_new[y] = str_1[i];
+      y++;
+    }
+    // check for whitespace
+    else if (str_1[i] == ' ') {
+      if (y > 0 && str_new[y - 1] != ' ') {
+        str_new[y] = ' ';
+        y++;
+      }
+    }
+    // all other characters
+    else {
+      str_new[y] = str_1[i];
+      y++;
+    }
+  }
+  return str_new; 
 }
+
+
+
+
+
+
+
+
+
+// #include <stdio.h>
+// #include <string.h>
+
+// char *noSpace(char* str_1);
+
+// char *noSpace(char* str_1){
+//   int L = strlen(str_1);
+//     char* str_new = malloc(1024);
+//     memset(str_new, '\0', 1024);
+//     char str_temp[1024]= "";
+//     int x = 0;
+//     int y= 0;
+//     for(int i = 0; i < L; i++) {
+//         if(str_1[i] == '>' || str_1[i] == '|'){
+//           if(str_1[i] == '>'){
+//             if((str_1[i-1] != ' ') || (str_1[i+1] != ' ')){
+//               str_temp[x] = ' ';
+//               x++;
+//             }
+//           }
+//           str_temp[x] = str_1[i];
+//           x++;
+//           if(str_1[i] == '>'){
+//             if((str_1[i-1] != ' ') || (str_1[i+1] != ' ')){
+//               str_temp[x] = ' ';
+//               x++;
+//             }
+//           }
+//         }
+//         else if(str_temp[x] == ' ' && str_1[i] == ' '){
+//             continue;
+//           }
+//         else { 
+//             str_temp[x] = str_1[i];
+//            // printf("%c", str_temp[x]);
+//             str_new[y] = str_temp[x];
+//             y++;
+//           }
+
+//     }
+
+//     //printf("%s\n", str_new);
+//     return str_new; 
+// }
 
 
 
