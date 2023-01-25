5e436c8b3a880acfcebfa1b9865cb5bf1f393d28
MohammadAhmed108 <122654720+MohammadAhmed108@users.noreply.github.com>
2023-01-24 19:24:03 -0800

    Update Whitespace.h

--- Whitespace.h
+++ Whitespace.h
@@ -3,8 +3,6 @@
 
 char *noSpace(char* str_1);
 
-
-
 char *noSpace(char* str_1){
   int L = strlen(str_1);
     char* str_new = malloc(1024);
@@ -13,7 +11,23 @@ char *noSpace(char* str_1){
     int x = 0;
     int y= 0;
     for(int i = 0; i < L; i++) {
-        if(str_temp[x] == ' ' && str_1[i] == ' '){
+        if(str_1[i] == '>' || str_1[i] == '|'){
+          if(str_1[i] == '>'){
+            if((str_1[i-1] != ' ') || (str_1[i+1] != ' ')){
+              str_temp[x] = ' ';
+              x++;
+            }
+          }
+          str_temp[x] = str_1[i];
+          x++;
+          if(str_1[i] == '>'){
+            if((str_1[i-1] != ' ') || (str_1[i+1] != ' ')){
+              str_temp[x] = ' ';
+              x++;
+            }
+          }
+        }
+        else if(str_temp[x] == ' ' && str_1[i] == ' '){
             continue;
           }
         else { 
@@ -28,3 +42,40 @@ char *noSpace(char* str_1){
     //printf("%s\n", str_new);
     return str_new; 
 }
+
+
+
+
+
+
+
+// #include <stdio.h>
+// #include <string.h>
+
+// char *noSpace(char* str_1);
+
+
+
+// char *noSpace(char* str_1){
+//   int L = strlen(str_1);
+//     char* str_new = malloc(1024);
+//     memset(str_new, '\0', 1024);
+//     char str_temp[1024]= "";
+//     int x = 0;
+//     int y= 0;
+//     for(int i = 0; i < L; i++) {
+//         if(str_temp[x] == ' ' && str_1[i] == ' '){
+//             continue;
+//           }
+//         else { 
+//             str_temp[x] = str_1[i];
+//            // printf("%c", str_temp[x]);
+//             str_new[y] = str_temp[x];
+//             y++;
+//           }
+
+//     }
+
+//     //printf("%s\n", str_new);
+//     return str_new; 
+// }
